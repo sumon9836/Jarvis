777cada77e88363b6ed3094432f034a30bb6ca01
Ronak sharma <112383311+Ronak-xd@users.noreply.github.com>
2025-05-29 21:12:22 +0530

    fix

--- plugins/client/media.js
+++ plugins/client/media.js
@@ -55,10 +55,48 @@ async function createRoundSticker(mediaBuffer) {
   return roundedSticker;
 };
 
+async function cropToCircle(input) {
+  try {
+    const image = sharp(input);
+    const { width, height } = await image.metadata();
+    const circleMask = Buffer.from(
+      `<svg width="${width}" height="${height}">
+         <circle cx="${width / 2}" cy="${height / 2}" r="${Math.min(width, height) / 2}" fill="white"/>
+       </svg>`
+    );
+
+    const croppedImage = await image
+      .composite([{ input: circleMask, blend: 'dest-in' }])
+      .toFormat('webp', { quality: 50 })
+      .toBuffer();
+
+    return croppedImage;
+  } catch (error) {
+    console.error('Error cropping image to circle:', error);
+    throw error;
+  }
+};
+
+async function cropImage(mediaBuffer) {
+  const image = sharp(mediaBuffer);
+  const metadata = await image.metadata();
+
+  const size = Math.min(metadata.width, metadata.height);
+  const left = Math.floor((metadata.width - size) / 2);
+  const top = Math.floor((metadata.height - size) / 2);
+
+  const croppedBuffer = await image
+    .extract({ left, top, width: size, height: size })
+    .toBuffer();
+
+  return croppedBuffer;
+};
 
 
 module.exports = { 
   trim, 
   audioCut,
+  cropImage,
+  cropToCircle,
   createRoundSticker
 };
